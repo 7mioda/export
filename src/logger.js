94e55887917b451c99a0bebd0ac06747b93ef74f
7mioda <ahmededaly1993@gmail.com>
2019-04-08 17:02:53 +0200

    fixing logger bug

--- src/logger.js
+++ src/logger.js
@@ -1,7 +1,16 @@
 import fs from 'fs';
 
-const logger = fs.createWriteStream('./logger.txt', { encoding: 'utf-8', flags: 'r+' });
-logger.on('open', () => console.log('Logger s ready'));
+
+let logger;
+fs.stat('./logger.txt', (error, stats) => {
+  if (error) {
+    fs.appendFileSync('./logger.txt', '');
+  }
+  logger = fs.createWriteStream('./logger.txt', { encoding: 'utf-8', flags: 'r+' });
+  logger.on('open', () => console.log('Logger s ready'));
+  return stats;
+});
+
 export const log = (data) => {
   logger.write((`${JSON.stringify(data, null, 2)}\n`));
 };
